2ca4406c86ad3e46a964942ba886ee2984ac5b9e
eamontracey <etracey@nd.edu>
2023-07-30 10:31:33 -0400

    add depth tables for edge groups

--- src/main.c
+++ src/main.c
@@ -4,9 +4,17 @@
 #include "table.h"
 
 int main() {
-    uint8_t *corners_depth_table = generate_depth_table(&corners_state, 88189740);
-    write_depth_table("corners.dt", corners_depth_table, 88189740);
-    free(corners_depth_table);
+    uint8_t *corners_dt = generate_depth_table(&corners_state, 88189740);
+    write_depth_table("corners.dt", corners_dt, 88189740);
+    free(corners_dt);
+
+    uint8_t *first_six_edges_dt = generate_depth_table(&first_six_edges_state, 42577920);
+    write_depth_table("first_six_edges.dt", first_six_edges_dt, 42577920);
+    free(first_six_edges_dt);
+
+    uint8_t *second_six_edges_dt = generate_depth_table(&second_six_edges_state, 42577920);
+    write_depth_table("second_six_edges.dt", second_six_edges_dt, 42577920);
+    free(second_six_edges_dt);
 
     return 0;
 }
--- src/state.h
+++ src/state.h
@@ -3,6 +3,8 @@
 
 #include "cube.h"
 
-uint32_t corners_state(struct cube *cube);
+uint32_t first_six_edges_state(struct cube *);
+uint32_t second_six_edges_state(struct cube *);
+uint32_t corners_state(struct cube *);
 
 #endif
