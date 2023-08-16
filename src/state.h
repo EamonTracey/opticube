6e3edf28c94cb43a783627f327faf28fce8ef3fc
eamontracey <etracey@nd.edu>
2023-08-15 21:52:35 -0500

    begin two phase solver

--- src/state.h
+++ src/state.h
@@ -5,6 +5,9 @@
 
 #include "cube.h"
 
+uint32_t eo_equator_combination_state(const struct cube *);
+uint32_t co_equator_combination_state(const struct cube *);
+
 uint32_t six_edges_a_state(const struct cube *);
 uint32_t six_edges_b_state(const struct cube *);
 uint32_t corners_state(const struct cube *);
